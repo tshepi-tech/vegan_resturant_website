e7bc05ac6c81da47b9b2ee8497175a01b40034af
tshepi_tech <tshepi.tech@gmail.com>
2022-03-09 15:17:31 +0100

    logo links back to homepage

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
     <nav>
-      <img src="" alt="logo of resturant" />
+      <Link to="/">
+        <img src="" alt="logo of resturant" />
+      </Link>
       <ul>
         <NavLink to="/Dishes">Dishes</NavLink>
         <NavLink to="/Drinks">Drinks</NavLink>

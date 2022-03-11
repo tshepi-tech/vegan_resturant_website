11a403d1bb2a0e4b75ba285cba88b6b2bb4a8f3c
tshepi_tech <tshepi.tech@gmail.com>
2022-03-11 08:24:03 +0100

    structuring product page

--- src/Pages/ProductTemplate.jsx
+++ src/Pages/ProductTemplate.jsx
@@ -5,10 +5,15 @@ export default function ProductTemplate() {
   let params = useParams();
   const { id } = params;
   const productItem = products.find((item) => item.id === id);
-
+  const ingredients = productItem.ingredients.map((item) => (
+    <li key={item.id} item={item}></li>
+  ));
+  console.log(productItem.ingredients);
   return (
     <div>
       <h3>{productItem.product}</h3>
+      <p>{productItem.detail}</p>
+      <ul>{ingredients}</ul>
     </div>
   );
 }

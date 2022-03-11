eae0804628b237f5ea6f61cc7f3df7ab8ced5279
tshepi_tech <tshepi.tech@gmail.com>
2022-03-11 15:50:38 +0100

    added ingredients & nutrition

--- src/Pages/ProductTemplate.jsx
+++ src/Pages/ProductTemplate.jsx
@@ -5,15 +5,33 @@ export default function ProductTemplate() {
   let params = useParams();
   const { id } = params;
   const productItem = products.find((item) => item.id === id);
-  const ingredients = productItem.ingredients.map((item) => (
-    <li key={item.id} item={item}></li>
-  ));
-  console.log(productItem.ingredients);
+
   return (
     <div>
       <h3>{productItem.product}</h3>
       <p>{productItem.detail}</p>
-      <ul>{ingredients}</ul>
+
+      <p>{productItem.ingredients[0].name}</p>
+      <p>{productItem.ingredients[1].name}</p>
+      <p>{productItem.ingredients[2].name}</p>
+      <p>{productItem.ingredients[3].name}</p>
+      <p>{productItem.ingredients[4].name}</p>
+      <table>
+        <tbody>
+          <tr>
+            <td>{productItem.nutrition[0].label}</td>
+            <td>{productItem.nutrition[1].label}</td>
+            <td>{productItem.nutrition[2].label}</td>
+            <td>{productItem.nutrition[3].label}</td>
+          </tr>
+          <tr>
+            <td>{productItem.nutrition[0].value}</td>
+            <td>{productItem.nutrition[1].value}</td>
+            <td>{productItem.nutrition[2].value}</td>
+            <td>{productItem.nutrition[3].value}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
